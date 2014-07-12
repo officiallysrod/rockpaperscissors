e042047ff33cc983211663ff063d5d46c109e013
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-07-12 12:33:47 -0700

    updated to return results in the window instead of an alert.

--- rps.js
+++ rps.js
@@ -23,35 +23,29 @@ function setComputerChoice() {
   compare();
 }
 
-// function playGame(userChoice) {
-//   user_choice = userChoice;
-//   rand_choice = Math.floor((Math.random() * 3) + 1);
-//   computer_choice = choices[rand_choice];
-//   compare();
-// }
-
 function compare() {
   if(user_choice != computer_choice) {
     if(user_choice == "rock" && computer_choice == "paper") {
-      alert("You picked " + user_choice + " and the computer picked " + computer_choice + ". Computer wins!");
+      document.getElementById("results").innerHTML = "Whammy! You lose.";
     }
     else if(user_choice == "rock" && computer_choice == "scissors") {
-      alert("You picked " + user_choice + " and the computer picked " + computer_choice + ". You win!");
+      document.getElementById("results").innerHTML = "Winner, winner. Chicken dinner.";
     }
     else if(user_choice == "paper" && computer_choice == "rock") {
-      alert("You picked " + user_choice + " and the computer picked " + computer_choice + ". You win!");
+      document.getElementById("results").innerHTML = "Winner, winner. Chicken dinner.";
     }
     else if(user_choice == "paper" && computer_choice == "scissors") {
-      alert("You picked " + user_choice + " and the computer picked " + computer_choice + ". Computer wins!");
+      document.getElementById("results").innerHTML = "Whammy! You lose.";
     }
     else if(user_choice == "scissors" && computer_choice == "rock") {
-      alert("You picked " + user_choice + " and the computer picked " + computer_choice + ". Computer wins!");
+      document.getElementById("results").innerHTML = "Whammy! You lose.";
     }
     else {
-      alert("You picked " + user_choice + " and the computer picked " + computer_choice + ". You win!");
+      document.getElementById("results").innerHTML = "Winner, winner. Chicken dinner.";
     }
   }
   else {
-    alert("You picked " + user_choice + " and the computer picked " + computer_choice + ". It's a tie!");
+    document.getElementById("results").innerHTML = "It's a tie.  How unsatisfying.";
   }
 }
+
