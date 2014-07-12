ff12476e4cafd00af226272093a9f6d43fd0c480
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-07-12 15:49:03 -0700

    refactored the compare function to remove redundant code

--- rps.js
+++ rps.js
@@ -2,6 +2,8 @@ var choices = [null, "rock", "paper", "scissors"];
 var letters = [null, "R", "P", "S"];
 var user_choice = null;
 var computer_choice = null;
+var user_score = 0;
+var computer_score = 0;
 
 function setUserChoice(userChoice, not1, not2) {
   user_choice = userChoice;
@@ -15,7 +17,7 @@ function setUserChoice(userChoice, not1, not2) {
 function playGame () {
   setComputerChoice();
   compare();
-  user_choice = null;
+  alert("User score: " + user_score + ", Computer score: " + computer_score);
 }
 
 function setComputerChoice() {
@@ -31,23 +33,13 @@ function setComputerChoice() {
 function compare() {
   if (user_choice != null) {  
     if(user_choice != computer_choice) {
-      if(user_choice == "rock" && computer_choice == "paper") {
-        document.getElementById("results").innerHTML = "Whammy! You lose.";
-      }
-      else if(user_choice == "rock" && computer_choice == "scissors") {
+      if((user_choice == "rock" && computer_choice == "scissors") || (user_choice == "paper" && computer_choice == "rock") || (user_choice == "scissors" && computer_choice == "paper")) {
         document.getElementById("results").innerHTML = "Winner, winner. Chicken dinner.";
-      }
-      else if(user_choice == "paper" && computer_choice == "rock") {
-        document.getElementById("results").innerHTML = "Winner, winner. Chicken dinner.";
-      }
-      else if(user_choice == "paper" && computer_choice == "scissors") {
-        document.getElementById("results").innerHTML = "Whammy! You lose.";
-      }
-      else if(user_choice == "scissors" && computer_choice == "rock") {
-        document.getElementById("results").innerHTML = "Whammy! You lose.";
+        user_score++;
       }
       else {
-        document.getElementById("results").innerHTML = "Winner, winner. Chicken dinner.";
+        document.getElementById("results").innerHTML = "Whammy! You lose.";
+        computer_score++;
       }
     }
     else {
@@ -64,12 +56,16 @@ function buttonStyle() {
 
 function playAgain() {
   //style resets. find a better way to globally reset styles to defaults.
-  document.getElementById("rock").style.backgroundColor = "red";
-  document.getElementById("paper").style.backgroundColor = "red";
-  document.getElementById("scissors").style.backgroundColor = "red";
-  document.getElementById("computerchoice").style.backgroundColor = "#20A9BA";
-  document.getElementById("letter").innerHTML = "?";
-  document.getElementById("results").innerHTML = "";
-  document.getElementById("playagain").style.backgroundColor = "white";
-  document.getElementById("playagain").innerHTML = "";
+  if(user_choice != null && computer_choice != null) {
+    document.getElementById("rock").style.backgroundColor = "red";
+    document.getElementById("paper").style.backgroundColor = "red";
+    document.getElementById("scissors").style.backgroundColor = "red";
+    document.getElementById("computerchoice").style.backgroundColor = "#20A9BA";
+    document.getElementById("letter").innerHTML = "?";
+    document.getElementById("results").innerHTML = "";
+    document.getElementById("playagain").style.backgroundColor = "white";
+    document.getElementById("playagain").innerHTML = "";
+    user_choice = null;
+    computer_choice = null;
+  }
 }
