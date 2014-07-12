b32aa632d11922b5ffcb18c3426d301cc134a9d9
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-07-11 17:30:38 -0700

    js is working now.  refactored the js into a single playGame function and updated the html to call the new playGame function.

--- rps.js
+++ rps.js
@@ -1,27 +1,36 @@
-var choices = [null, "rock", "paper", "scissors"]
+var choices = [null, "rock", "paper", "scissors"];
 var user_choice = null;
 var computer_choice = null;
 
-function userRock() {
-  user_choice = "rock";
-}
-
-function userPaper() {
-  user_choice = "paper";
-}
-
-function userScissors() {
-  user_choice = "scissors";
-}
-
-function computerChoice() {
+function playGame(userChoice) {
+  user_choice = userChoice;
   rand_choice = Math.floor((Math.random() * 3) + 1);
   computer_choice = choices[rand_choice];
-  alert(computer_choice);
+  compare();
 }
 
 function compare() {
-  if user_choice != computer_choice {
-    
+  if(user_choice != computer_choice) {
+    if(user_choice == "rock" && computer_choice == "paper") {
+      alert("You picked " + user_choice + " and the computer picked " + computer_choice + ". Computer wins!");
+    }
+    else if(user_choice == "rock" && computer_choice == "scissors") {
+      alert("You picked " + user_choice + " and the computer picked " + computer_choice + ". You win!");
+    }
+    else if(user_choice == "paper" && computer_choice == "rock") {
+      alert("You picked " + user_choice + " and the computer picked " + computer_choice + ". You win!");
+    }
+    else if(user_choice == "paper" && computer_choice == "scissors") {
+      alert("You picked " + user_choice + " and the computer picked " + computer_choice + ". Computer wins!");
+    }
+    else if(user_choice == "scissors" && computer_choice == "rock") {
+      alert("You picked " + user_choice + " and the computer picked " + computer_choice + ". Computer wins!");
+    }
+    else {
+      alert("You picked " + user_choice + " and the computer picked " + computer_choice + ". You win!");
+    }
+  }
+  else {
+    alert("You picked " + user_choice + " and the computer picked " + computer_choice + ". It's a tie!");
   }
 }
