f0e15f572b8953d0728a30490f5d56ee98608a30
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-07-14 17:49:05 -0700

    javascript and css refactored.  implemented visibility: hidden attribute.  no new features.

--- rps.js
+++ rps.js
@@ -1,6 +1,6 @@
 //initialize variables.
-var choices = [null, "rock", "paper", "scissors"];
-var letters = [null, "R", "P", "S"];
+var choices = ["rock", "paper", "scissors"];
+var letters = ["R", "P", "S"];
 var user_choice = null;
 var computer_choice = null;
 var user_score = 0;
@@ -8,11 +8,8 @@ var computer_score = 0;
 
 //sets user_choice variable and affects background colors based on user click.
 function setUserChoice(userChoice, not1, not2) {
-  if(computer_choice === null) {  
-    user_choice = userChoice;
-    not1 = not1;
-    not2 = not2;
-    document.getElementById(user_choice).style.backgroundColor = "#20A9BA";
+  if(computer_choice === null) {
+    document.getElementById(userChoice).style.backgroundColor = "#20A9BA";
     document.getElementById(not1).style.backgroundColor = "#DEDEDD";
     document.getElementById(not2).style.backgroundColor = "#DEDEDD";
   }
@@ -29,7 +26,7 @@ function playGame() {
 //and changes background color of the element to red.
 function setComputerChoice() {
   if(user_choice !== null && computer_choice === null) {
-    var rand_choice = Math.floor((Math.random() * 3) + 1);
+    var rand_choice = Math.floor(Math.random() * 3);
     computer_choice = choices[rand_choice];
     var letter = letters[rand_choice];
     document.getElementById("computerchoice").style.backgroundColor = "red";
@@ -41,28 +38,26 @@ function setComputerChoice() {
 //computer_choice variables to declare a winner.  also calls the buttonStyle() function.
 function compare() {
   if(user_choice !== null) {
-    if(user_choice != computer_choice) {
-      if((user_choice == "rock" && computer_choice == "scissors") || (user_choice == "paper" && computer_choice == "rock") || (user_choice == "scissors" && computer_choice == "paper")) {
-        document.getElementById("results").innerHTML = "Winner, winner. Chicken dinner.";
-        user_score++;
-      }
-      else {
-        document.getElementById("results").innerHTML = "Whammy! You lose.";
-        computer_score++;
-      }
+    if(user_choice == computer_choice) {
+      document.getElementById("results").innerHTML = "It's a tie.  How unsatisfying.";
+    }
+    else if((user_choice == "rock" && computer_choice == "scissors") ||
+     (user_choice == "paper" && computer_choice == "rock") ||
+     (user_choice == "scissors" && computer_choice == "paper")) {
+      document.getElementById("results").innerHTML = "Winner, winner. Chicken dinner.";
+      user_score++;
     }
     else {
-      document.getElementById("results").innerHTML = "It's a tie.  How unsatisfying.";
+      document.getElementById("results").innerHTML = "Whammy! You lose.";
+      computer_score++;
     }
     buttonStyle();
   }
 }
 
-//called by the compare() function.  changes background color of button from 
-//white to red to make the button visible on screen.  also adds the text "PLAY AGAIN" to the button.
+//called by the compare() function.
 function buttonStyle() {
-  document.getElementById("playagain").style.backgroundColor = "red";
-  document.getElementById("playagain").innerHTML = "PLAY AGAIN";
+  document.getElementById("playagain").style.visibility = "visible";
 }
 
 //this function is called when the user clicks on "play again" button.
@@ -75,8 +70,7 @@ function playAgain() {
   document.getElementById("computerchoice").style.backgroundColor = "#20A9BA";
   document.getElementById("letter").innerHTML = "?";
   document.getElementById("results").innerHTML = "";
-  document.getElementById("playagain").style.backgroundColor = "white";
-  document.getElementById("playagain").innerHTML = "";
+  document.getElementById("playagain").style.visibility = "hidden";
   user_choice = null;
   computer_choice = null;
 }
